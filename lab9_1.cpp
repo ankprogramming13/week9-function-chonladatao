b955c86fec404176a2c861349e4e65f8fa0b8132
Unknown <changyom12.teas@gmail.com>
2018-01-19 13:41:50 +0700

    ทำข้อ 1 เมื่อสักครู่ส่งผิด
    ทำฟังก์ชัน

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
 using namespace std;
+void swapNumber (int *x, int *y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void showvalues(int x, int y){
+        cout << "This is a : " << x << endl;
+        cout << "This is a : " << y << endl;
+}
+
+
 
 int main() {
     /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
@@ -8,6 +21,20 @@ int main() {
     และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
     */
 
+        int a,b;
+        cin >> a;
+        cin >> b;
+        showvalues(a, b);
+
+        int cmd;
+        do{
+                cin >> cmd;
+                if (cmd == 1){
+                        swapNumber(&a, &b);
+                        showvalues(a, b);
+                }
+
+        }while(cmd != 2);
 
 
     return 0;
